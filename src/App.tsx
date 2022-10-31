362c7a08eb52155ec0e5077082e25c8476d74ee6
Oluwafolajimi Olaosebikan <folagmie16@gmail.com>
2022-10-31 16:09:59 -0700

    index

--- src/App.tsx
+++ src/App.tsx
@@ -104,9 +104,9 @@ const App : React.FC = () => {
           </Button>
         </Container>
       </Modal>
-      {allTodo.map(({ description, name }, ind) => {
+      {allTodo.map(({ description, name }, index) => {
         return (
-          <React.Fragment key={ind}>
+          <React.Fragment key={index}>
             <CustomCard
               name={name}
               description={description}
